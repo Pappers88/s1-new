68a0a420a46cf792e8f8b4f198a33bfe3f2c78dd
Martyn Papprill <martyn_papprill@hotmail.co.uk>
2017-04-26 12:04:44 +0100

    Add files via upload
    first commit

---
+++ s1-alternate/s1.js
@@ -0,0 +1,19 @@
+jQuery(function ($) {
+        function init_map1() {
+            var myLocation = new google.maps.LatLng(51.8423000, 0.7114370);
+            var mapOptions = {
+                center: myLocation,
+                zoom: 16
+            };
+            var marker = new google.maps.Marker({
+                position: myLocation,
+                title: "Property Location"
+            });
+            var map = new google.maps.Map(document.getElementById("map1"),
+                mapOptions);
+            marker.setMap(map);
+        }
+        init_map1();
+    });
+
+
